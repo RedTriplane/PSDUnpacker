972eee020d0819da139b84d9492dd4e64ffd6621
J Fixby <github@jfixby.com>
2016-04-02 13:30:16 +0200

    +ByteArray, +UTF-String, +abstracArray

--- jfixby-tool-psd-unpacker-red/jfixby-tools-psd-unpacker-core/com/jfixby/psd/unpacker/core/RedPSDUnpacker.java
+++ jfixby-tool-psd-unpacker-red/jfixby-tools-psd-unpacker-core/com/jfixby/psd/unpacker/core/RedPSDUnpacker.java
@@ -12,37 +12,36 @@ import com.jfixby.psd.unpacker.core.legacy.PSDReader;
 
 public class RedPSDUnpacker implements PSDUnpackerComponent {
 
-	@Override
-	public PSDUnpackingParameters newUnpackingSpecs() {
-		return new PSDUnpackingParametersImpl();
+    @Override
+    public PSDUnpackingParameters newUnpackingSpecs() {
+	return new PSDUnpackingParametersImpl();
+    }
+
+    @Override
+    public PSDFileContent unpack(PSDUnpackingParameters params) throws IOException {
+	boolean crash_on_mask = params.getCrashOnMask();
+
+	File file = params.getPSDFile();
+
+	L.d("unpacking", file);
+
+	if (!file.exists()) {
+	    throw new IOException("File not found: " + file);
 	}
 
-	@Override
-	public PSDFileContent unpack(PSDUnpackingParameters params)
-			throws IOException {
-		boolean crash_on_mask = params.getCrashOnMask();
-
-		File file = params.getPSDFile();
-
-		L.d("unpacking", file);
-
-		if (!file.exists()) {
-			throw new IOException("File not found: " + file);
-		}
-
-		if (!file.isFile()) {
-			throw new IOException("This is not a file: " + file);
-		}
-
-		PSDReader unpacker = new PSDReader();
-		unpacker.setCrashOnMask(crash_on_mask);
-
-		byte[] bytes = file.readBytes();
-
-		com.jfixby.psd.unpacker.core.legacy.FileContent result = unpacker
-				.readFromStream(file.getName(), new ByteArrayInputStream(bytes));
-
-		return new PSDFileContentImpl(result);
+	if (!file.isFile()) {
+	    throw new IOException("This is not a file: " + file);
 	}
+
+	PSDReader unpacker = new PSDReader();
+	unpacker.setCrashOnMask(crash_on_mask);
+
+	byte[] bytes = file.readBytes().toArray();
+
+	com.jfixby.psd.unpacker.core.legacy.FileContent result = unpacker.readFromStream(file.getName(),
+		new ByteArrayInputStream(bytes));
+
+	return new PSDFileContentImpl(result);
+    }
 
 }
